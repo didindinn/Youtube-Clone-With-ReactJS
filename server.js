aca050f23ae9fb43f55da92aca1a38d752615960
MeRichard123 <rcoric03@gmail.com>
2019-12-25 15:40:09 +0000

    add files

--- server.js
+++ server.js
@@ -1,29 +1,8 @@
-const { createServer } = require("https");
-var express = require("express");
-const compression = require("compression");
+const express = require("express");
 const path = require("path");
-
-var port = process.env.PORT || 5000;
-
-var app = express();
-const dev = app.get("env") !== "production";
-if (!dev) {
-  app.disable("x-powered-by");
-  app.use(compression());
-  app.use(morgan("common"));
-
-  app.use(express.static(__dirname));
-
-  app.use(express.static(path.resole(__dirname, "build")));
-
-  app.get("*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
-
-const server = createServer(app);
-
-server.listen(port, err => {
-  if (err) throw err;
-  console.log("app running");
+const app = express();
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+app.listen(9000);
