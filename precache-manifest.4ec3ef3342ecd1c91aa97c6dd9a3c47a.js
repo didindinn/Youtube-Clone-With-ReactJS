682927f8088f1ea3610ca704591ae55974c3432a
MeRichard123 <rcoric03@gmail.com>
2019-12-25 18:36:50 +0000

    Updates

---
+++ precache-manifest.4ec3ef3342ecd1c91aa97c6dd9a3c47a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0487a01d0ba3ba7b76dcd8121af51cfc",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "3966ba293ca745b7b837",
+    "url": "/youtube-clone/static/css/main.c67934dd.chunk.css"
+  },
+  {
+    "revision": "62c905ef531b783a2536",
+    "url": "/youtube-clone/static/js/2.d106f4ce.chunk.js"
+  },
+  {
+    "revision": "04b21ccc9109def6e71671ed6f2d794a",
+    "url": "/youtube-clone/static/js/2.d106f4ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3966ba293ca745b7b837",
+    "url": "/youtube-clone/static/js/main.47b7f820.chunk.js"
+  },
+  {
+    "revision": "62238df7476bb14b7fb2",
+    "url": "/youtube-clone/static/js/runtime-main.2274eb46.js"
+  }
+]);
