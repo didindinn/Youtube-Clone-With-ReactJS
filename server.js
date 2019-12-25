0bb0bbefcba54b75652de8f2dd53b86f331e8fde
MeRichard123 <rcoric03@gmail.com>
2019-12-25 15:19:54 +0000

    files

--- server.js
+++ server.js
@@ -1,33 +1,29 @@
-const {createServer} = require("https")
-var express = require("express")
-const compression = require("compression")
-const morgan = require("morgan")
-const path = require("path")
+const { createServer } = require("https");
+var express = require("express");
+const compression = require("compression");
+const path = require("path");
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 5000;
 
 var app = express();
-const dev = app.get("env") !== "production"
-if(!dev){
-  app.disable("x-powered-by")
-  app.use(compression())
-  app.use(morgan("common"))
-  
+const dev = app.get("env") !== "production";
+if (!dev) {
+  app.disable("x-powered-by");
+  app.use(compression());
+  app.use(morgan("common"));
+
   app.use(express.static(__dirname));
- 
 
-  app.use(express.static(path.resole(__dirname,'build')));
+  app.use(express.static(path.resole(__dirname, "build")));
 
-  app.get("*",function(req,res){
-    res.sendFile(path.resolve(__dirname,"build","index.html"))
-  })
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 }
-if(dev){
-  app.use(morgan("dev"))
-}
-const server = createServer(app)
 
-server.listen(port,err=>{
-  if(err) throw err;
-  console.log("app running")
-}
+const server = createServer(app);
+
+server.listen(port, err => {
+  if (err) throw err;
+  console.log("app running");
+});
